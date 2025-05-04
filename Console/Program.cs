89e80d5eec0a5c14c97774f2e1c9f33078d11023
PrivatePuma <lsantoro@students.ccu.edu>
2025-05-03 20:48:16 -0600

    Final Submission Commit

---
+++ Console/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+using Spectre.Console;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.CursorVisible = false;
+        string url = "http://cis-230-sp25.azurewebsites.net/api/StringArray"; // Replace with your desired URL
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string responseText = await client.GetStringAsync(url);
+                responseText = responseText.Trim('[', ']');
+                string[] responseArray = responseText.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                WriteResponse(responseArray);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine($"JSON error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+            Console.ReadKey(true);
+        }
+    }
+
+    private static void WriteResponse(string[] response)
+    {
+        for (int i = 1; i < response.Length; i++)
+        {
+            string color = GetColor(i);
+            AnsiConsole.MarkupLine($"[{color}]{i}. {response[i]}[/]");
+        }
+    }
+
+    private static string GetColor(int index)
+    {
+        while (index > 9)
+        {
+            index -= 9;
+        }
+        return index switch
+        {
+            1 => "red",
+            2 => "#FE9900",
+            3 => "#FCF649",
+            4 => "green",
+            5 => "cyan",
+            6 => "blue",
+            7 => "purple",
+            8 => "magenta",
+            9 => "#FF01F2",
+            _ => "white"
+        };
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CIS_230_SP25_Week16
 LS CIS-230 Week 16 coding assignment submission
+
+Hi Professor Nixon :D
